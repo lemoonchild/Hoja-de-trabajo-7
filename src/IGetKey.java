6655067c932163a861524e1b2a67bc3287066896
Nahomy Castro <ncastrom21@gmail.com>
2023-03-23 21:15:37 -0600

    Inicio de clases iniciales, se añadieron txt y metodos para su lectura.

---
+++ src/IGetKey.java
@@ -0,0 +1,7 @@
+/**
+ * @author MAAG
+ *
+ */
+public interface IGetKey<K, V> {
+	K getKeyFromValue(V value);
+}
---
+++ src/ITraversal.java
@@ -0,0 +1,9 @@
+/**
+ * @author MAAG
+ *
+ */
+public interface ITraversal<K, V> {
+
+	void visit(TreeNode<K, V> actualNode);
+	
+}
