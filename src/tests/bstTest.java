92eed63e4cb40d42572e1c14a5e230943bd22e39
Nahomy Castro <ncastrom21@gmail.com>
2023-03-25 15:37:50 -0600

    Se añadieron pruebas de JUnit para insertar y buscar datos dentro del arbol de busqueda binaria

---
+++ src/tests/bstTest.java
@@ -0,0 +1,41 @@
+package tests;
+
+import Controller.compareWords;
+import Controller.controllerDictionary;
+import Controller.extractMainWord;
+import Controller.readFile;
+import Model.BinarySearchTree;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * The type Binary tree test.
+ */
+class BinaryTreeTest {
+
+    compareWords compare = new compareWords();
+    extractMainWord mainWord = new extractMainWord<>();
+    BinarySearchTree testTree = new BinarySearchTree(compare,mainWord);
+
+
+
+    @Test
+    public void testInsertAndSearch() {
+
+        testTree.insert("dog", "perro");
+        testTree.insert("woman", "mujer");
+        testTree.insert("cat", "gato");
+        testTree.insert("apple", "manzana");
+
+
+        assertEquals("perro", testTree.search("dog").toString());
+        assertEquals("mujer", testTree.search("woman").toString());
+        assertEquals("gato", testTree.search("cat").toString());
+        assertEquals("manzana", testTree.search("apple").toString());
+
+    }
+}
